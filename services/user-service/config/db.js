2310d62ca038d2fdcd691ca8c7f90d1354b4b0ca
Tanishq Gupta <tanishq.gupta@tax2win.in>
2025-07-22 16:57:21 +0530

    Debugging

--- services/user-service/config/db.js
+++ services/user-service/config/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 
 const DATABASE_URL = `postgres:[${process.env.DB_PASSWORD}]@db.usghgpcjuvcjhijoiwlx.supabase.co:5432/postgres`;
+coonsole.log(DATABASE_URL);
 const sequelize = new Sequelize(DATABASE_URL);
 module.exports = sequelize;
