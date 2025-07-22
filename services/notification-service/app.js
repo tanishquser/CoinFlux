bd915a08a23f5217546d75d1171f0959166e254c
Tanishq Gupta <tanishq.gupta@tax2win.in>
2025-07-22 18:30:19 +0530

    Notification port added

--- services/notification-service/app.js
+++ services/notification-service/app.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const startConsumer = require('./consumers/notification.consumer');
 
 startConsumer(); // No Express server needed for now
+
+const PORT = process.env.NOTIFICATION_PORT || 3004;
+app.listen(PORT, () => {
+  console.log(`🚀 Wallet Service running at http://localhost:${PORT}`);
+});
